07e981254d1fa9930aa262fbc75d4a383b5e8f52
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2026-03-04 09:27:39 -0800

    change resend.ts back

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,20 +1,3 @@
 import { Resend } from "resend";
 
-const resendClient = new Resend(process.env.RESEND_API_KEY || "");
-
-const emailsEnabled = process.env.SEND_EMAILS !== "false";
-
-export const resend = {
-  emails: {
-    send: async (params: Parameters<typeof resendClient.emails.send>[0]) => {
-      if (!emailsEnabled) {
-        console.log("[Email skipped - SEND_EMAILS=false]", {
-          to: params.to,
-          subject: params.subject,
-        });
-        return { data: null, error: null };
-      }
-      return resendClient.emails.send(params);
-    },
-  },
-};
+export const resend = new Resend(process.env.RESEND_API_KEY || "");
